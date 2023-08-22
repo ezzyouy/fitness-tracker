81073361b2847a6b6c56ed7b30416f3b82f86712
ezzyouy <82385007+ezzyouy@users.noreply.github.com>
2023-08-22 12:38:19 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCL3-ywdvWWAr6wCqCb5aeh2Zis09EGg_I',
+    apiKey: '***************',
     authDomain: 'ng-fitness-trakcer-35ae5.firebaseapp.com',
     projectId: 'ng-fitness-trakcer-35ae5',
     storageBucket: 'ng-fitness-trakcer-35ae5.appspot.com',
-    messagingSenderId: '258850745566',
-    appId: '1:258850745566:web:5942c225d8b133fa85264f'
+    messagingSenderId: '***************',
+    appId: '***************'
   }
 }
